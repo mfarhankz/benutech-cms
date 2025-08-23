139b51c853467b45bc114e2d4f1e2a3c9115d7cb
Muhammad Farhan <m.farhankz@gmail.com>
2025-08-24 04:38:05 +0500

    Initial commit: Benutech CMS setup

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,29 @@
+import clsx from 'clsx'
+import React from 'react'
+
+interface Props {
+  className?: string
+  loading?: 'lazy' | 'eager'
+  priority?: 'auto' | 'high' | 'low'
+}
+
+export const Logo = (props: Props) => {
+  const { loading: loadingFromProps, priority: priorityFromProps, className } = props
+
+  const loading = loadingFromProps || 'lazy'
+  const priority = priorityFromProps || 'low'
+
+  return (
+    /* eslint-disable @next/next/no-img-element */
+    <img
+      alt="Payload Logo"
+      width={217}
+      height={74}
+      loading={loading}
+      fetchPriority={priority}
+      decoding="async"
+      className={clsx('', className)}
+      src="/benutach-logo.png"
+    />
+  )
+}
